59c2ef1064847352be1a9f9ee3a69bb8c8ffa725
praveen9900 <praveenpatil4666@gmail.com>
2025-11-08 23:27:31 +0530

    spring-ai code is added

---
+++ src/main/java/dev/danvega/workshop/SimpleChatController.java
@@ -0,0 +1,25 @@
+package dev.danvega.workshop;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleChatController {
+
+    private final ChatClient chatClient;
+
+    public SimpleChatController(ChatClient.Builder builder) {
+        this.chatClient = builder
+                .build();
+    }
+
+    @GetMapping("/boeing")
+    public String chat() {
+        return chatClient.prompt()
+                .user("Tell me an intresting fact about Beoing")
+                .call()
+                .content();
+    }
+
+}
---
+++ src/main/java/dev/danvega/workshop/rag/ModelsController.java
@@ -0,0 +1,31 @@
+package dev.danvega.workshop.rag;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ModelsController {
+
+    private final ChatClient chatClient;
+
+    public ModelsController(ChatClient.Builder builder, VectorStore vectorStore) {
+        this.chatClient = builder
+                .defaultAdvisors(new QuestionAnswerAdvisor(vectorStore))
+                .build();
+    }
+
+    @GetMapping("/rag/models")
+    public Models faq(@RequestParam(value = "message", defaultValue = "Give me a list of all the models from OpenAI along with their context window.") String message) {
+        return chatClient.prompt()
+                .user(message)
+                .call()
+                .entity(Models.class);
+    }
+
+}
